c79ae83963c25f2c5cdd60a95c5b7ab7e0d93588
Abhishek Verma <abhishekverma6533@gmail.com>
2024-01-07 21:06:49 +0530

    Add Note Completes

--- src/component/AddNote.js
+++ src/component/AddNote.js
@@ -1,63 +1,73 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 import Notes from "./Notes";
-import noteContext from '../context/note/NoteContext';
+import noteContext from "../context/note/NoteContext";
 
 function AddNote() {
-    const context = useContext(noteContext);
-    const {addNote} = context;
+  const context = useContext(noteContext);
+  const { addNote } = context;
 
-    const[note,SetNote] = useState("")
+  const [note, SetNote] = useState({ title: "", description: "", tag: "default"});
 
-    const handleClick =() =>{
-          console.log(addNote);
-    }
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(addNote);
+    addNote(note.title , note.description , note.tag);
+  };
 
-    const onChange=(e) =>{
-      SetNote({...note ,[e.target.name] : e.target.value})
-    }
+  const onChange = (e) => {
+    SetNote({ ...note, [e.target.name]: e.target.value });
+  };
   return (
     <div>
       <div className="container my-2">
-      <h1>Add a Note</h1>
-      <form className="my-4">
-        <div className="mb-3">
-          <label htmlFor="title" className="form-label">
-            Title
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="title"
-            name='title'
-            aria-describedby="emailHelp"
-            onChange={onChange}
-          />
-          
-
-        </div>
-        <div className="mb-3">
-          <label htmlFor="desc" className="form-label">
-            Description
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="desc" name='desc'
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1" >
-            Check me out
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary" onClick={handleClick} >
-          Submit
-        </button>
-      </form>
+        <h1>Add a Note</h1>
+        <form className="my-4">
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              name="title"
+              aria-describedby="emailHelp"
+              onChange={onChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+              name="description"
+              onChange={onChange}
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleClick}
+          >
+            Add Note
+          </button>
+        </form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default AddNote
+export default AddNote;
--- src/context/note/NoteState.js
+++ src/context/note/NoteState.js
@@ -2,96 +2,100 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-   const initialNotes =
-    [
-      {
-        "_id": "659533cef907426dd2b1eed5",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Abhishek verma ",
-        "description": "Be consistent abhi",
-        "tag": "personal",
-        "date": "2024-01-03T10:15:42.405Z",
-        "__v": 0
-      },
-      {
-        "_id": "65968ac7c2ec1551e320f1da",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Abhishek Verma Abhi",
-        "description": "This is updated description",
-        "tag": "Personal Updated",
-        "date": "2024-01-04T10:39:03.961Z",
-        "__v": 0
-      },
-      {
-        "_id": "65968ac8c2ec1551e320f1dc",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Harry Bhai",
-        "description": "Thanks Harry Bhai for your react tutorial",
-        "tag": "Tutorial",
-        "date": "2024-01-04T10:39:04.105Z",
-        "__v": 0
-      },
-      {
-        "_id": "65968ac8c2ec1551e320f1de",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Harry Bhai",
-        "description": "Thanks Harry Bhai for your react tutorial",
-        "tag": "Tutorial",
-        "date": "2024-01-04T10:39:04.277Z",
-        "__v": 0
-      },
-      {
-        "_id": "65968ac8c2ec1551e320f1e0",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Harry Bhai",
-        "description": "Thanks Harry Bhai for your react tutorial",
-        "tag": "Tutorial",
-        "date": "2024-01-04T10:39:04.473Z",
-        "__v": 0
-      },
-      {
-        "_id": "65968ac8c2ec1551e320f1e2",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Harry Bhai",
-        "description": "Thanks Harry Bhai for your react tutorial",
-        "tag": "Tutorial",
-        "date": "2024-01-04T10:39:04.661Z",
-        "__v": 0
-      },
-      {
-        "_id": "65968ac8c2ec1551e320f1e4",
-        "user": "6591dfe8d9e320fe637e1fcb",
-        "title": "Harry Bhai",
-        "description": "Thanks Harry Bhai for your react tutorial",
-        "tag": "Tutorial",
-        "date": "2024-01-04T10:39:04.839Z",
-        "__v": 0
-      },
-    ]
+  const initialNotes = [
+    {
+      _id: "659533cef907426dd2b1eed5",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Abhishek verma ",
+      description: "Be consistent abhi",
+      tag: "personal",
+      date: "2024-01-03T10:15:42.405Z",
+      __v: 0,
+    },
+    {
+      _id: "65968ac7c2ec1551e320f1da",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Abhishek Verma Abhi",
+      description: "This is updated description",
+      tag: "Personal Updated",
+      date: "2024-01-04T10:39:03.961Z",
+      __v: 0,
+    },
+    {
+      _id: "65968ac8c2ec1551e320f1dc",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Harry Bhai",
+      description: "Thanks Harry Bhai for your react tutorial",
+      tag: "Tutorial",
+      date: "2024-01-04T10:39:04.105Z",
+      __v: 0,
+    },
+    {
+      _id: "65968ac8c2ec1551e320f1de",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Harry Bhai",
+      description: "Thanks Harry Bhai for your react tutorial",
+      tag: "Tutorial",
+      date: "2024-01-04T10:39:04.277Z",
+      __v: 0,
+    },
+    {
+      _id: "65968ac8c2ec1551e320f1e0",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Harry Bhai",
+      description: "Thanks Harry Bhai for your react tutorial",
+      tag: "Tutorial",
+      date: "2024-01-04T10:39:04.473Z",
+      __v: 0,
+    },
+    {
+      _id: "65968ac8c2ec1551e320f1e2",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Harry Bhai",
+      description: "Thanks Harry Bhai for your react tutorial",
+      tag: "Tutorial",
+      date: "2024-01-04T10:39:04.661Z",
+      __v: 0,
+    },
+    {
+      _id: "65968ac8c2ec1551e320f1e4",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: "Harry Bhai",
+      description: "Thanks Harry Bhai for your react tutorial",
+      tag: "Tutorial",
+      date: "2024-01-04T10:39:04.839Z",
+      __v: 0,
+    },
+  ];
 
-    const [notes, setNotes] = useState(initialNotes);
+  const [notes, setNotes] = useState(initialNotes);
 
-    // Add a Note
-    const addNote =(note)=>{
-      setNotes(notes.push(note))
+  // Add a Note
+  const addNote = (title,description,tag) => {
+    console.log("adding a new note");
+    const note={
+      _id: "65968ac7c2ec1551e320f1da",
+      user: "6591dfe8d9e320fe637e1fcb",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2024-01-04T10:39:03.961Z",
+      __v: 0,
     }
-   
-    // Add a Note
-    const deleteNote =(id)=>{
+    setNotes(notes.concat(note));
+  };
 
-    }
+  // Add a Note
+  const deleteNote = (id) => {};
 
-    // Add a Note
-    const editNote =(id)=>{
-
-    }
+  // Add a Note
+  const editNote = (id) => {};
 
   return (
-  <NoteContext.Provider  value={{notes, addNote, deleteNote , editNote}}>
- 
-   { props.children}
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+      {props.children}
     </NoteContext.Provider>
-  )
+  );
 };
 
 export default NoteState;
